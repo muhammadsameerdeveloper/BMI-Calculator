7e811ba7b90ef20b78f2d9f9d631040c726e4e3e
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-09-06 21:48:55 +0500

    Gender Screen

--- lib/screen/gender_selection_view.dart
+++ lib/screen/gender_selection_view.dart
@@ -1,10 +1,38 @@
+import 'package:bmiapp/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GenderSelectionView extends StatelessWidget {
   const GenderSelectionView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: Column(
+        children: [
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "BMI",
+                  style: GoogleFonts.roboto(
+                    color: AppColors.orangeColor,
+                    fontSize: 20,
+                  ),
+                ),
+                TextSpan(
+                  text: "Calculator",
+                  style: GoogleFonts.roboto(
+                    color: AppColors.greenColor,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/widgets.dart';
 
 class AppColors {
   static Color greenColor = Color(0xff65B741);
+  static Color whiteColor = Color(0xffF5F5F5);
+  static Color orangeColor = Color(0xffFFB534);
 }
